76d9cc47ca9c74609c8ac6a1e8e68d8e6b2d99bd
ansang <sho989434@naver.com>
2022-02-11 14:22:16 +0900

    text Style, color Uitl 추가

---
+++ lib/utils/textStyle.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TextStyle_Util {
+
+  TextStyle s_20_blue = TextStyle(
+    fontSize: 20,
+    color: Colors.blue
+  );
+
+  TextStyle s_16_red = TextStyle(
+      fontSize: 16,
+      color: Colors.red
+  );
+
+  TextStyle s_15_teal = TextStyle(
+      fontSize: 15,
+      color: Colors.teal
+  );
+}
