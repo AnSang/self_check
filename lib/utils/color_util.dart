51a70f461136d34f02b81ddaa3f8d9f7a9c3cc28
ansang <sho989434@naver.com>
2022-02-16 18:38:04 +0900

    색상 추가

--- lib/utils/color_util.dart
+++ lib/utils/color_util.dart
@@ -6,4 +6,5 @@ class Color_Util {
   static const Color activeColor = Color(0xFF09126C);
   static const Color textColor1 = Color(0xFFA7BCC7);
   static const Color backgroundColor = Color(0xFFECF3F9);
+  static const Color googleColor = Color(0xFFDE4B39);
 }
