6b06bab0dca7303ca1d5aefa48fa10444879b309
ansang <sho989434@naver.com>
2022-02-21 15:51:02 +0900

    기상시간 관리 화면 수정

---
+++ lib/ui/second_wake.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Second_wake extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          Text('Second_wake')
+        ],
+      ),
+    );
+  }
+}
