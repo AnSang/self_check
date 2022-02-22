3dfc7a062123ebc44e20fa338600bc3cd32874b6
ansang <sho989434@naver.com>
2022-02-22 15:08:17 +0900

    BottomNavigation 유저정보 화면 생성

---
+++ lib/ui/bottom_userinfo.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Bottom_UserInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('This is User Info Screen');
+  }
+}
