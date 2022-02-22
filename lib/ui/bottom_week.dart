e75dde6d9b3c3a4a2cfff62bf0bba6914ceddda4
ansang <sho989434@naver.com>
2022-02-22 15:10:05 +0900

    BottomNavigation 일주일 통계 화면 생성

---
+++ lib/ui/bottom_week.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Bottom_Weather extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('This is Weather Screen');
+  }
+}
