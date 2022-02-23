7a4af278868f2f242ed5110061cae3da40ae26b7
ansang <sho989434@naver.com>
2022-02-23 18:50:48 +0900

    WeightController 몸무게 저장 및 불러오기 구현

--- lib/controller/weight_controller.dart
+++ lib/controller/weight_controller.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:self_check/models/weight.dart';
 
 class WeightController extends GetxController {
+  static const key = 'weigth';
+
+  late SharedPreferences pref;
+  List<Weight>? items;
+
   var isInput = false;
   final textController = TextEditingController();
 
   @override
-  void onInit() {
+  void onInit() async {
     textController.text = '0.0';
+    pref = await SharedPreferences.getInstance();
+    items = initItems();
+    super.onInit();
   }
 
   @override
@@ -26,5 +38,35 @@ class WeightController extends GetxController {
       return 'Please this field must be filled';
     }
     return '';
+  }
+
+
+  // preferences
+
+
+  List<Weight> initItems() {
+    String? _list = pref.getString(key);
+    if (_list == null) {
+      return <Weight>[];  // 없으면 빈 list 반환
+    } else {
+      List list = jsonDecode(_list!);
+      return list.cast<Weight>();
+    }
+  }
+
+  Weight getItem(int index) {
+    return items![index];
+  }
+
+  void addItemRow(Weight item) {
+    items!.add(item);
+  }
+
+  void addItemData(String date, double weight) {
+    items!.add(Weight(date: date, weight: weight));
+    pref.setString(key, jsonEncode(items!));
+
+    items!.clear();
+    items = initItems();
   }
 }
